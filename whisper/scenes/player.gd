600edcdb4ec141f59cef62f840809ae92a76f776
Onyx <loesvanvlerken2012@gmail.com>
2026-03-28 23:48:59 +0100

    the movement system now works good, and  i made a background

--- whisper/scenes/player.gd
+++ whisper/scenes/player.gd
@@ -1,25 +1,14 @@
 extends CharacterBody2D
 
+const SPEED = 130.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	direction = direction.normalized()
+	velocity = direction * SPEED
+	
 	move_and_slide()
